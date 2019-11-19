f0353bf982ae7ef2b215c50d03cdd59ce901641e
AimAlex <alex-li@sjtu.edu.cn>
2019-11-18 17:58:37 -0800

    add test for polygon export

--- app/src/test/server/export.test.ts
+++ app/src/test/server/export.test.ts
@@ -3,7 +3,8 @@ import _ from 'lodash'
 import Session from '../../js/common/session'
 import { State } from '../../js/functional/types'
 import { convertItemToExport, convertStateToExport } from '../../js/server/export'
-import { sampleItemExportImage, sampleStateExportImage } from '../test_export_objects'
+import { sampleItemExportImage, sampleItemExportImagePolygon,
+         sampleStateExportImage, sampleStateExportImagePolygon} from '../test_export_objects'
 
 beforeAll(() => {
   Session.devMode = false
@@ -12,17 +13,31 @@ beforeAll(() => {
 describe('test export functionality across multiple labeling types', () => {
   describe('test export functionality for bounding box', () => {
     test('single item conversion', () => {
-      const state = readSampleState()
+      const state = readSampleState('./app/src/test/sample_state.json')
       const config = state.task.config
       const item = state.task.items[0]
       const itemExport = convertItemToExport(config, item)[0]
       expect(itemExport).toEqual(sampleItemExportImage)
     }),
       test('full state export with empty items', () => {
-        const state = readSampleState()
+        const state = readSampleState('./app/src/test/sample_state.json')
         const exportedState = convertStateToExport(state)
         expect(exportedState).toEqual(sampleStateExportImage)
       })
+  }),
+  describe('test export functionality for segmentation', () => {
+    test('single item conversion', () => {
+      const state = readSampleState('./app/src/test/sample_state_polygon.json')
+      const config = state.task.config
+      const item = state.task.items[0]
+      const itemExport = convertItemToExport(config, item)[0]
+      expect(itemExport).toEqual(sampleItemExportImagePolygon)
+    }),
+    test('full state export with empty items', () => {
+      const state = readSampleState('./app/src/test/sample_state_polygon.json')
+      const exportedState = convertStateToExport(state)
+      expect(exportedState).toEqual(sampleStateExportImagePolygon)
+    })
   }),
   describe('test export functionality for tracking', () => {
     test('single item conversion', () => {
@@ -37,6 +52,6 @@ describe('test export functionality across multiple labeling types', () => {
 /**
  * helper function to read sample state
  */
-function readSampleState (): State {
-  return JSON.parse(fs.readFileSync('./app/src/test/sample_state.json', 'utf8'))
+function readSampleState (fileName: string): State {
+  return JSON.parse(fs.readFileSync(fileName, 'utf8'))
 }
