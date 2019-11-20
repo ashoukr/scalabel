570453cf84c6e4479d2152256023baac4f9755ff
Gary Chen <gary@datu.ai>
2019-11-20 14:19:14 -0800

    Move split pane style from CSS to JSS

--- app/src/js/components/label_pane.tsx
+++ app/src/js/components/label_pane.tsx
@@ -1,9 +1,11 @@
+import { createStyles } from '@material-ui/core'
 import * as React from 'react'
 import SplitPane from 'react-split-pane'
 import { updateAll } from '../action/common'
 import Session from '../common/session'
 import { PaneType } from '../functional/types'
 import ViewerContainer, { viewerContainerReactKey } from './viewer_container'
+import { withStyles } from '@material-ui/styles'
 
 /**
  * Wrapper for SplitPane
@@ -58,4 +60,47 @@ class LabelPane extends React.Component<PaneType> {
   }
 }
 
-export default LabelPane
+const styles = () => createStyles({
+  Resizer: {
+    'background': 'rgba(150, 150, 150, 1)',
+    'opacity': 0.2,
+    'zIndex': 1,
+    '-moz-box-sizing': 'border-box',
+    '-webkit-box-sizing': 'border-box',
+    'box-sizing': 'border-box',
+    '-moz-background-clip': 'padding',
+    '-webkit-background-clip': 'padding',
+    'background-clip': 'padding-box',
+    '&:hover': {
+      '-webkit-transition': 'all 2s ease',
+      'transition': 'all 2s ease'
+    },
+    '&.horizontal': {
+      'height': '11px',
+      'margin': '-5px 0',
+      'border-top': '5px solid rgba(255, 255, 255, 0)',
+      'border-bottom': '5px solid rgba(255, 255, 255, 0)',
+      'cursor': 'row-resize',
+      'width': '100%'
+    },
+    '&.vertical': {
+      'width': '11px',
+      'margin': '0 -5px',
+      'border-left': '5px solid rgba(255, 255, 255, 0)',
+      'border-right': '5px solid rgba(255, 255, 255, 0)',
+      'cursor': 'col-resize',
+      '&:hover': {
+        'border-top': '5px solid rgba(100, 100, 100, 1)',
+        'border-bottom': '5px solid rgba(100, 100, 100, 1)'
+      }
+    },
+    '&.disabled': {
+      'cursor': 'not-allowed',
+      '&:hover': {
+        'border-color': 'transparent'
+      }
+    }
+  }
+})
+
+export default withStyles(styles, { withTheme: true })(LabelPane)
